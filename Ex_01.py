71a3d483c686c017c5488a5c6a9c1bd0aff25488
AlbertoParraToval <alberto.parra.fp@iescampanillas.com>
2023-11-25 12:40:30 +0100

    First Commit

---
+++ Ex_01.py
@@ -0,0 +1,5 @@
+# Utilizando la clasificación vista en clase sobre los lenguajes de programación, escoge 5
+# lenguajes que desees y clasifícalos en una tabla según su nivel de abstracción, su forma
+# de ejecución y los paradigmas de programación que incorpora. No olvides incluir el
+# año de aparición y el autor/autores del mismo como MÍNIMO. Incluye toda esta
+# información en un fichero llamado lenguajes.pdf.
