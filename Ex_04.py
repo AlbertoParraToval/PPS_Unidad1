effbf311ba5f906dbc5268f32633c90865c935eb
AlbertoParraToval <alberto.parra.fp@iescampanillas.com>
2023-11-25 12:46:08 +0100

    Enunciados Subidos

---
+++ Ex_04.py
@@ -0,0 +1,3 @@
+# Crea una suite de tests mediante UnitTest que comprueben las 3 funciones que has
+# desarrollado en los ejercicios anteriores. Procura que los tests unitarios cubran lo
+# mejor posible la aparición de comportamientos no deseados. 
---
+++ Ex_05.py
@@ -0,0 +1,2 @@
+# Realiza el ejercicio 4 pero utilizando esta vez cualquier otro framework de terceros
+# como por ejemplo pytest.
